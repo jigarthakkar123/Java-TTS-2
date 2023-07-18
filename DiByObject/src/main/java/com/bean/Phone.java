846a94dff61705de5d687cbef0c287a378c70b2c
jigarthakkar123 <jigar.thakkar.tops@gmail.com>
2023-07-18 16:17:51 +0530

    18-7-23

---
+++ DiByObject/src/main/java/com/bean/Phone.java
@@ -0,0 +1,18 @@
+package com.bean;
+
+public class Phone {
+
+	private String pno;
+
+	
+	
+	public Phone(String pno) {
+		this.pno = pno;
+	}
+
+
+
+	public void call() {
+		System.out.println("Calling A Number...."+pno);
+	}
+}
---
+++ SpringHello/src/main/java/com/spring/SpringHello/App.java
@@ -0,0 +1,22 @@
+package com.spring.SpringHello;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.bean.Student;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        AbstractApplicationContext a=new ClassPathXmlApplicationContext("Beans.xml");
+        Student s1=a.getBean("s2",Student.class);
+        System.out.println(s1);
+        a.registerShutdownHook();
+    }
+}
