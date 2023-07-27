acb8dc320ddd17f89d87ac05901861532fbee18e
jigarthakkar123 <jigar.thakkar.tops@gmail.com>
2023-07-27 16:23:57 +0530

    27-7-23

---
+++ SpringMVC/src/main/java/com/bean/Student.java
@@ -0,0 +1,45 @@
+package com.bean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="SPRING_MVC")
+public class Student {
+
+	@Id
+	@GenericGenerator(name = "inc",strategy = "increment")
+	@GeneratedValue(generator = "inc")
+	private int id;
+	private String fname,lname,email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
---
+++ SpringORM/src/main/java/com/bean/Student.java
@@ -0,0 +1,56 @@
+package com.bean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="Spring_ORM")
+public class Student {
+
+	private int id;
+	String fname,lname,email,mobile;
+	
+	@Id
+	@GenericGenerator(name="inc",strategy = "increment")
+	@GeneratedValue(generator = "inc")
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	@Column(name="FNAME")
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	@Column(name="LNAME")
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	@Column(name="EMAIL")
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Column(name="MOBILE")
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	
+}
